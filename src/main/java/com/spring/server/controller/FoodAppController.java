5fd75a4362c9536bc1b82f6e7bd2941cf721c83c
Felix Steinke <steinke.felix@yahoo.de>
2020-11-15 11:47:25 +0100

    added cross orgin (coors) support

--- src/main/java/com/spring/server/controller/FoodAppController.java
+++ src/main/java/com/spring/server/controller/FoodAppController.java
@@ -8,14 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import jsonUtility.JsonBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class FoodAppController {
 
