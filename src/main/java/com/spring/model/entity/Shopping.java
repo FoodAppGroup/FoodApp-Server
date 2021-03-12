617bbfea0f2d26ead8c6c805035104ffea62bf02
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-03-12 11:14:25 +0100

    update apimodel

--- src/main/java/com/spring/model/entity/Shopping.java
+++ src/main/java/com/spring/model/entity/Shopping.java
@@ -1,5 +1,7 @@
 package com.spring.model.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,7 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@ApiModel
 @Entity(name = "ShoppingList")
 @Table(name = "shopping_list")
 public class Shopping implements Serializable {
@@ -17,10 +19,12 @@ public class Shopping implements Serializable {
     @EmbeddedId
     private Key key;
 
+    @ApiModelProperty(value = "Linked product.", example = "Apfel")
     @ManyToOne
     @JoinColumn(name = "product_name", referencedColumnName = "name", insertable = false, updatable = false)
     private Product product;
 
+    @ApiModelProperty(value = "Number of the amount in the Shopping List.", example = "2")
     @Column(name = "number", nullable = false)
     private Integer number;
 
@@ -29,15 +33,16 @@ public class Shopping implements Serializable {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @ApiModel
     @Embeddable
     public static class Key implements Serializable {
 
+        @ApiModelProperty(value = "List name.", example = "Standardliste")
         @Column(name = "list_name", nullable = false)
         private String listName;
+
+        @ApiModelProperty(value = "Linked product name.", example = "Apfel")
         @Column(name = "product_name", nullable = false)
         private String productName;
     }
-
 }
-
-
